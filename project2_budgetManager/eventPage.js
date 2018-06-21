010cfad6b213f7994a68308f9dd8eb4db6b2d47d
Zone <zonetw.web@gmail.com>
2018-06-21 21:57:28 +0800

    add functionality: add cost that selected
    will show notification when reach the limit

--- project2_budgetManager/eventPage.js
+++ project2_budgetManager/eventPage.js
@@ -6,3 +6,37 @@ var contextMenuSetting = {
 }
 
 chrome.contextMenus.create(contextMenuSetting);
+
+
+function isInt(input){
+    let result = !isNaN(input) && parseInt(Number(input)) == Number(input) && !isNaN(parseInt(input, 10));
+    return result;
+}
+
+chrome.contextMenus.onClicked.addListener((clickedData)=>{
+    if(clickedData.menuItemId == contextMenuSetting.id){
+        let  selectionText = clickedData.selectionText
+        if(selectionText && isInt(selectionText)){
+            chrome.storage.sync.get(["total", "limit"], (result)=>{
+                let oldTotal = result.total || 0;
+                let selectCost = parseInt(selectionText);
+                let limit = result.limit || 0;
+                let newTotal = oldTotal + selectCost;
+                if(newTotal > limit){
+                    //spend too much, just notify, don't record
+                    chrome.notifications.create({
+                        type: "basic",
+                        iconUrl: "broccoli.png",
+                        title: "Limit Reach",
+                        message: "you spend too much"
+                    });
+                }
+
+                chrome.storage.sync.set({
+                    total: newTotal
+                });                
+            });
+        }
+    }
+    
+});
