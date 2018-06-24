31abf1914649382e054e1505fed5f283f8221fed
Zone <zonetw.web@gmail.com>
2018-06-24 17:27:20 +0800

    pick color and apply to api class

--- project3_changePageFontStyle/content.js
+++ project3_changePageFontStyle/content.js
@@ -1,1 +1,8 @@
 chrome.runtime.sendMessage({action: "showPage"});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
+    if(request.todo === "changeColor"){
+        let addColor = "#" + request.changedColor;
+        $(".api").css("color", addColor);
+    }
+});
