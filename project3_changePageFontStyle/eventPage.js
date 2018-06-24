188f9ffc15e5dd17ee68af3531e684a4e2478b62
Zone <zonetw.web@gmail.com>
2018-06-24 09:41:21 +0800

    content.js tell eventPage to show

--- project3_changePageFontStyle/eventPage.js
+++ project3_changePageFontStyle/eventPage.js
@@ -1,3 +1,8 @@
-chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
-    chrome.pageAction.show(tabs[0].id);
+chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
+    if(request.action === "showPage"){
+        //show extension
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
+            chrome.pageAction.show(tabs[0].id);
+        });
+    }
 });
